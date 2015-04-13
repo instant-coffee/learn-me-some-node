c7e9431f4798d55d4908068beb0b48fb30256ee1
instant-coffee <lachlanrm@gmail.com>
2015-04-13 23:49:14 +0000

    Question 8

---
+++ 08_http_collect.js
@@ -0,0 +1,17 @@
+
+var http = require('http')
+var bl = require('bl')
+
+var url = process.argv[2];
+
+http.get(url, function (res) {
+  res.pipe(bl(function (err, data) { 
+    if (err)
+      return console.errror(err);
+    data = data.toString();
+    console.log(data.length);
+    console.log(data);
+  }));
+});
+
+ 
